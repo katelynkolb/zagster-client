0227b11699804b805d47811d2c408a3f950e1564
Katelyn Kolb <kolbk@oregonstate.edu>
2018-11-16 10:56:27 -0800

    added G5 ride count

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ $(updateView)
 
 function updateView()  {
      $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+     $.getJSON(BASE_URL + "/rides/count/g5", updateG5Count)
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
+}
+
+function updateG5Count(data)  {
+    numberOfRides = data.count
+    $("p#G5").html(numberOfRides)
 }
 
 // function printdata(data){
