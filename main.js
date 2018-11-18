afcffe516f63a681d8df23793630e41313071c6d
Katelyn Kolb <kolbk@oregonstate.edu>
2018-11-18 12:25:54 -0800

    added drake park

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function updateView()  {
      $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
      $.getJSON(BASE_URL + "/rides/count/g5", updateG5Count)
      $.getJSON(BASE_URL + "/rides/count/columbia_simpson", updatecolumbia_simpsonCount)
+     $.getJSON(BASE_URL + "/rides/count/drake_park", updatedrake_parkCount)
 }
 
 function updateRideCount(data) {
@@ -23,6 +24,11 @@ function updateG5Count(data)  {
 function updatecolumbia_simpsonCount(data)  {
     numberOfRides = data.count
     $("p#Columbia_Simpson").html(numberOfRides)
+}
+
+function updatedrake_parkCount(data)  {
+    numberOfRides = data.count
+    $("p#drake_park").html(numberOfRides)
 }
 
 // function printdata(data){
